cb186fa5e8bfcc8f3699d5c9085253f360d717d1
Ricardo Sparapan <ricardo.ofice@hotmail.com>
2020-04-21 20:15:35 -0300

    Adding hash password

--- src/routes/users.routes.ts
+++ src/routes/users.routes.ts
@@ -10,6 +10,7 @@ usersRoutes.post('/', async (request, response) => {
 
     const user = await createUser.run({ name, email, password });
 
+    delete user.password;
     return response.json(user);
   } catch (err) {
     return response.status(400).json({ error: err.message });
