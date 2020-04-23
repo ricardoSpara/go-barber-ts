abb08abdb954091e71ccb574b0553e06ab912d31
Ricardo Sparapan <ricardo.ofice@hotmail.com>
2020-04-22 21:22:18 -0300

    adding auth service

---
+++ src/routes/sessions.routes.ts
@@ -0,0 +1,22 @@
+import { Router } from 'express';
+import AuthenticateUserService from '../services/AuthenticateUserService';
+
+const sessionsRoutes = Router();
+
+sessionsRoutes.post('/', async (request, response) => {
+  try {
+    const { email, password } = request.body;
+
+    const authenticateUser = new AuthenticateUserService();
+
+    const { user } = await authenticateUser.run({ email, password });
+
+    delete user.password;
+
+    return response.json({ user });
+  } catch (err) {
+    return response.status(400).json({ error: err.message });
+  }
+});
+
+export default sessionsRoutes;
