ec15129f9ad9d69620a2388f1e0d8d7df0aa2e43
Ricardo Sparapan <ricardo.ofice@hotmail.com>
2020-04-25 17:36:06 -0300

    Class AppError

--- src/services/AuthenticateUserService.ts
+++ src/services/AuthenticateUserService.ts
@@ -3,6 +3,7 @@ import { compare } from 'bcryptjs';
 import { sign } from 'jsonwebtoken';
 import User from '../models/User';
 import authConfig from '../config/auth';
+import AppError from '../errors/AppError';
 
 interface Request {
   email: string;
@@ -23,13 +24,13 @@ class AuthenticateUserService {
     });
 
     if (!user) {
-      throw new Error('Incorrect email/password combination.');
+      throw new AppError('Incorrect email/password combination.', 401);
     }
 
     const passowrdMatched = await compare(password, user.password);
 
     if (!passowrdMatched) {
-      throw new Error('Incorrect email/password combination.');
+      throw new AppError('Incorrect email/password combination.', 401);
     }
     const { secret, expiresIn } = authConfig.jwt;
     const token = sign({}, secret, {
