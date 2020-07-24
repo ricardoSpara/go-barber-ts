ce43249c3e77f0d84f24356c2c57a295349bc251
Ricardo Sparapan <ricardo.ofice@hotmail.com>
2020-07-23 22:53:54 -0300

    Validation fields

--- frontent/src/pages/SignUp/index.tsx
+++ frontent/src/pages/SignUp/index.tsx
@@ -1,6 +1,8 @@
-import React from 'react';
+import React, { useCallback } from 'react';
 import { FiArrowLeft, FiMail, FiUser, FiLock } from 'react-icons/fi';
 import { Form } from '@unform/web';
+import * as Yup from 'yup';
+
 import { Container, Content, Background } from './styles';
 import logoImg from '../../assets/logo.svg';
 
@@ -8,9 +10,21 @@ import Input from '../../components/Input';
 import Button from '../../components/Button';
 
 const SignUp: React.FC = () => {
-    function handlerSubmit(data: object): void {
-        console.log(data);
-    }
+    const handlerSubmit = useCallback(async (data: object) => {
+        try {
+            const schema = Yup.object().shape({
+                name: Yup.string().required('Nome obrigatório'),
+                email: Yup.string()
+                    .required('E-mail obrigatório')
+                    .email('Digite um e-mail válido'),
+                password: Yup.string().min(6, 'No mínimo 6 digitos'),
+            });
+
+            await schema.validate(data, { abortEarly: false });
+        } catch (err) {
+            console.log(err);
+        }
+    }, []);
 
     return (
         <Container>
