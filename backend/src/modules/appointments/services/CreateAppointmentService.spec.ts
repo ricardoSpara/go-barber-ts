3980d4e7a9b37870ff15677b481e1008fab8cba5
Ricardo Sparapan <ricardo.ofice@hotmail.com>
2020-08-31 21:45:04 -0300

    feat: setup jest

--- backend/src/modules/appointments/services/CreateAppointmentService.spec.ts
+++ backend/src/modules/appointments/services/CreateAppointmentService.spec.ts
@@ -1,3 +1,11 @@
-test('sum to numbers', () => {
-  expect(2 + 2).toBe(4);
+import CreateAppointmentService from './CreateAppointmentService';
+
+describe('CreateAppointment', () => {
+  it('should be able to create a new appointment', () => {
+    expect(1 + 2).toBe(3);
+  });
+
+  it('should not be able to create two appointment on the same time', () => {
+    expect(1 + 2).toBe(3);
+  });
 });
