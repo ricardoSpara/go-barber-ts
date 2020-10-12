394009437523e4b7f8020bf6be400ac8a67ef59c
Ricardo Sparapan <ricardo.ofice@hotmail.com>
2020-10-12 20:20:26 -0300

    feat: show providers

--- backend/src/shared/infra/http/routes/index.ts
+++ backend/src/shared/infra/http/routes/index.ts
@@ -4,6 +4,7 @@ import sessionsRoutes from '@modules/users/infra/http/routes/sessions.routes';
 import appointmentsRoutes from '@modules/appointments/infra/http/routes/appointments.routes';
 import passwordRoutes from '@modules/users/infra/http/routes/password.routes';
 import profileRoutes from '@modules/users/infra/http/routes/profile.routes';
+import providerRoutes from '@modules/appointments/infra/http/routes/providers.routes';
 
 const routes = Router();
 
@@ -11,6 +12,7 @@ routes.use('/appointments', appointmentsRoutes);
 routes.use('/users', usersRoutes);
 routes.use('/password', passwordRoutes);
 routes.use('/sessions', sessionsRoutes);
-routes.use('/profiles', profileRoutes);
+routes.use('/profile', profileRoutes);
+routes.use('/providers', providerRoutes);
 
 export default routes;
