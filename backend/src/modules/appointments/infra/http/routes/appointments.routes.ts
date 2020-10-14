66d6c70af563a190fb04e56add1a5a8f7599938b
Ricardo Sparapan <ricardo.ofice@hotmail.com>
2020-10-14 17:34:47 -0300

    feat: list appointments od provider

--- backend/src/modules/appointments/infra/http/routes/appointments.routes.ts
+++ backend/src/modules/appointments/infra/http/routes/appointments.routes.ts
@@ -2,16 +2,15 @@ import { Router } from 'express';
 
 import ensureAuthenticated from '@modules/users/infra/http/middlewares/ensureAuthenticated';
 import AppointmentsContoller from '../controllers/AppointmentsContoller';
+import ProviderAppointmentsController from '../controllers/ProviderAppointmentsController';
 
 const appointmentsRoutes = Router();
 const appointmentsContoller = new AppointmentsContoller();
+const providerAppointmentsController = new ProviderAppointmentsController();
 
 appointmentsRoutes.use(ensureAuthenticated);
 
-// appointmentsRoutes.get('/', async (request, response) => {
-//   const appointments = await appointmentsRepository.find();
-//   return response.json(appointments);
-// });
+appointmentsRoutes.get('/me', providerAppointmentsController.index);
 
 appointmentsRoutes.post('/', appointmentsContoller.create);
 
